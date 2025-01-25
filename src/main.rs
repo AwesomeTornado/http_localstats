e3a12cb44aec6085516ad894ef1ef23b38920f62
AwesomeTornado <harleypeterson@gmail.com>
2025-01-25 14:49:06 -0600

    MANY more methods

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use rouille::Request;
 use rouille::Response;
 use sysinfo::{
-    Components, Disks, Networks, System,
+    Components, Disks, Networks, System, CpuRefreshKind, RefreshKind
 };
 
 
@@ -37,11 +37,48 @@ fn main(){
             },
             "/global_cpu_usage" =>{
                 let system_quantity = sys.global_cpu_usage();
+                // Wait a bit because CPU usage is based on diff.
+                std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+                // Refresh CPUs again to get actual value.
+                sys.refresh_cpu_usage();
                 let response_string = system_quantity.to_string();
+                Response::text(response_string)
+            },
+            "/segmented_cpu_usage" =>{
+                let system_quantity = sys.cpus();
+                // Wait a bit because CPU usage is based on diff.
+                std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+                // Refresh CPUs again to get actual value.
+                sys.refresh_cpu_usage();
+                let mut response_string = "".to_string();
+                for cpu in sys.cpus() {
+                    println!("{}%", cpu.cpu_usage());
+                    response_string += (cpu.cpu_usage().to_string().as_str().to_owned() + "%,").as_str();
+                }
                 Response::text(response_string)
             },
             "/uptime" =>{
                 let system_quantity = sysinfo::System::uptime();
+                let response_string = system_quantity.to_string();
+                Response::text(response_string)
+            },
+            "/physical_core_count" =>{
+                let system_quantity = sys.physical_core_count().unwrap_or("ERROR".parse().unwrap());
+                let response_string = system_quantity.to_string();
+                Response::text(response_string)
+            },
+            "/name" =>{
+                let system_quantity = sysinfo::System::name().unwrap_or("ERROR".parse().unwrap());
+                let response_string = system_quantity.to_string();
+                Response::text(response_string)
+            },
+            "/verbose_os_version" =>{
+                let system_quantity = sysinfo::System::long_os_version().unwrap_or("ERROR".parse().unwrap());
+                let response_string = system_quantity.to_string();
+                Response::text(response_string)
+            },
+            "/cpu_arch" =>{
+                let system_quantity = sysinfo::System::cpu_arch();
                 let response_string = system_quantity.to_string();
                 Response::text(response_string)
             },
@@ -52,7 +89,7 @@ fn main(){
                 Response::text("OwO")
             },
             "/index" => {
-                Response::text("/ram_total, /ram_used, /swap_total, /swap_used, /cpu_total, /global_cpu_usage")
+                Response::text("/ram_total, /ram_used, /swap_total, /swap_used, /cpu_total, /global_cpu_usage, /uptime, /owo, /uwu, /segmented_cpu_usage, /physical_core_count, /name, /verbose_os_version, /cpu_arch, ")
             }
             _ =>{
                 Response::empty_404()
